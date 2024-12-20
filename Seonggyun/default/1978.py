613a0a19e0741e2560a9331929943bac0759ffa2
hskhyl <kannakawai9@gmail.com>
2024-12-20 22:14:54 +0900

    1day 1commit

---
+++ Seonggyun/default/1978.py
@@ -0,0 +1,14 @@
+# import sys
+
+# num = int(input())
+
+# numbers = list(map(int,sys.stdin.readline().strip().split()))
+# print(numbers)
+# answer = 0
+
+# for number in numbers:
+#     if number % 2 != 0 and number != 1:
+#         answer += 1
+
+# print(answer)
+    
